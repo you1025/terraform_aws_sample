e05da7d40dcb6518a80f01719d8c45d12750279c
You Shimajiro <you1025@gmail.com>
2023-06-24 16:22:42 +0900

    Subnet および RouteTable の追加

--- network.tf
+++ network.tf
@@ -1,4 +1,7 @@
-# vpc
+###################
+# VPC
+###################
+
 resource "aws_vpc" "vpc" {
   cidr_block                       = "192.168.0.0/16"
   instance_tenancy                 = "default"
@@ -11,4 +14,110 @@ resource "aws_vpc" "vpc" {
     Project     = var.project
     Environment = var.environment
   }
+}
+
+
+###################
+# Subnet
+###################
+
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-subnet-1a"
+    Project     = var.project
+    Environment = var.environment
+    type        = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1c"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-subnet-1c"
+    Project     = var.project
+    Environment = var.environment
+    type        = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1a"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-subnet-1a"
+    Project     = var.project
+    Environment = var.environment
+    type        = "private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "us-east-1c"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-subnet-1c"
+    Project     = var.project
+    Environment = var.environment
+    type        = "private"
+  }
+}
+
+
+###################
+# Route Table
+###################
+
+# public
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-rt"
+    Project     = var.project
+    Environment = var.environment
+    type        = "public"
+  }
+}
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+# private
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-rt"
+    Project     = var.project
+    Environment = var.environment
+    type        = "private"
+  }
+}
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
 }
