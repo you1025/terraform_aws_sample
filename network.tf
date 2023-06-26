52236ffff634e7b41ce265b574c012a66057ee23
You Shimajiro <you1025@gmail.com>
2023-06-26 17:01:47 +0900

    ネットワーク構成を変更

--- network.tf
+++ network.tf
@@ -35,42 +35,14 @@ resource "aws_subnet" "public_subnet_1a" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "${var.region}c"
-  cidr_block              = "192.168.2.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "${var.project}-${var.environment}-public-subnet-1c"
-    Project     = var.project
-    Environment = var.environment
-    type        = "public"
-  }
-}
-
 resource "aws_subnet" "private_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "${var.region}a"
-  cidr_block              = "192.168.3.0/24"
+  cidr_block              = "192.168.2.0/24"
   map_public_ip_on_launch = false
 
   tags = {
     Name        = "${var.project}-${var.environment}-private-subnet-1a"
-    Project     = var.project
-    Environment = var.environment
-    type        = "private"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "${var.region}c"
-  cidr_block              = "192.168.4.0/24"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name        = "${var.project}-${var.environment}-private-subnet-1c"
     Project     = var.project
     Environment = var.environment
     type        = "private"
@@ -97,10 +69,6 @@ resource "aws_route_table_association" "public_rt_1a" {
   route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.public_subnet_1a.id
 }
-resource "aws_route_table_association" "public_rt_1c" {
-  route_table_id = aws_route_table.public_rt.id
-  subnet_id      = aws_subnet.public_subnet_1c.id
-}
 
 # private
 resource "aws_route_table" "private_rt" {
@@ -116,10 +84,6 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_rt_1a" {
   route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private_subnet_1a.id
-}
-resource "aws_route_table_association" "private_rt_1c" {
-  route_table_id = aws_route_table.private_rt.id
-  subnet_id      = aws_subnet.private_subnet_1c.id
 }
 
 
