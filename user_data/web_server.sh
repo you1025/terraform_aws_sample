906a453a536f1474d1f55c0b59890758143695e2
You Shimajiro <you1025@gmail.com>
2023-07-01 17:55:20 +0900

    wordpress を導入

--- user_data/web_server.sh
+++ user_data/web_server.sh
@@ -2,5 +2,11 @@
 
 sudo dnf update -y
 sudo dnf -y install httpd
+sudo dnf -y install php8.1 php-mysqli
+
+wget https://ja.wordpress.org/wordpress-6.2.2-ja.tar.gz
+tar xzf wordpress-latest-ja.tar.gz
+sudo cp -r wordpress/* /var/www/html/
+sudo chown -R apache:apache /var/www/html
 
 sudo systemctl start httpd.service
