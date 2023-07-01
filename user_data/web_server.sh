840ba1f61fd76a41c83a4bd28dea93e83a597a93
You Shimajiro <you1025@gmail.com>
2023-07-01 21:29:57 +0900

    EC2 の初期設定を変更

--- user_data/web_server.sh
+++ user_data/web_server.sh
@@ -4,9 +4,9 @@ sudo dnf update -y
 sudo dnf -y install httpd
 sudo dnf -y install php8.1 php-mysqli
 
-wget https://ja.wordpress.org/wordpress-6.2.2-ja.tar.gz
-tar xzf wordpress-latest-ja.tar.gz
-sudo cp -r wordpress/* /var/www/html/
+sudo wget -P /tmp https://ja.wordpress.org/wordpress-latest-ja.tar.gz
+sudo tar xzf /tmp/wordpress-latest-ja.tar.gz -C /tmp
+sudo cp -r /tmp/wordpress/* /var/www/html
 sudo chown -R apache:apache /var/www/html
 
 sudo systemctl start httpd.service
