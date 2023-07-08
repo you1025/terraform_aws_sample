1674516f9e0077bce180eea3a273ee3e2022c314
You Shimajiro <you1025@gmail.com>
2023-07-08 15:16:46 +0900

    html ファイルのパスを変更

--- user_data/web_server.sh
+++ user_data/web_server.sh
@@ -3,5 +3,5 @@
 sudo dnf update -y
 sudo dnf -y install httpd
 
-sudo wget -O /var/www/html/index.html https://raw.githubusercontent.com/you1025/terraform_aws_sample/main/garage/hello.html
+sudo wget -O /var/www/html/index.html https://raw.githubusercontent.com/you1025/terraform_aws_sample/main/html/hello.html
 sudo systemctl start httpd.service
