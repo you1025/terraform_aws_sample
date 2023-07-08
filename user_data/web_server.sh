cf36fbdccaa7ed73d9a8f18007abf77234444291
You Shimajiro <you1025@gmail.com>
2023-07-08 15:15:43 +0900

    ファイルの位置を変更

---
+++ user_data/web_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo dnf update -y
+sudo dnf -y install httpd
+
+sudo wget -O /var/www/html/index.html https://raw.githubusercontent.com/you1025/terraform_aws_sample/main/garage/hello.html
+sudo systemctl start httpd.service
