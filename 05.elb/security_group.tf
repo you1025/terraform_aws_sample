57c2f19a8934041d7d82df0fdb7a3bf923cf9d3b
You Shimajiro <you1025@gmail.com>
2023-08-07 14:48:11 +0900

    ec2 への HTTP アクセスを elb だけに制限

--- 05.elb/security_group.tf
+++ 05.elb/security_group.tf
@@ -15,13 +15,13 @@ resource "aws_security_group" "web" {
   }
 }
 resource "aws_security_group_rule" "web_in_http" {
-  security_group_id = aws_security_group.web.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "${var.project}-${var.environment}-web-in-http-rule"
+  security_group_id        = aws_security_group.web.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.alb.id
+  description              = "${var.project}-${var.environment}-web-in-http-rule"
 }
 resource "aws_security_group_rule" "web_in_ssh" {
   security_group_id = aws_security_group.web.id
@@ -40,15 +40,6 @@ resource "aws_security_group_rule" "web_out_http" {
   to_port           = 80
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "${var.project}-${var.environment}-web-out-http-rule"
-}
-resource "aws_security_group_rule" "web_out_https" {
-  security_group_id = aws_security_group.web.id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "${var.project}-${var.environment}-web-out-https-rule"
 }
 
 # ALB
